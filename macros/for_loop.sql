130d48851d74015e63c788b5ae19d92f81772122
Ecequiel Chavez <ecequiel.chavez@dbtlabs.com>
2026-02-05 18:22:26 +0000

    fixed

--- macros/for_loop.sql
+++ macros/for_loop.sql
@@ -1,5 +1,19 @@
 {% set foods = ["radish", "cucumber", "chicken nugget", "avocado"] %}
 
-{% for food in foods %} 
-    The delicious {{ food }}
+{%- for food in foods -%} 
+    {%- if food == "chicken nugget" -%}
+        {% set food_type = "snack" %}
+    {%- else -%}
+        {%- set food_type = "vegetable" -%}
+    
+    {%- endif -%}
+    The delicious {{ food }} is my favorite {{ food_type}}
 {% endfor %}
+
+{%- set zeke_dictionary = {
+    'word': 'data',
+    'part_of_speech': 'noun',
+    'definition': 'the building block of life'
+} -%}
+
+{{ zeke_dictionary['word'] }}
