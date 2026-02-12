faa1f7e84f8b91f6973a1ca6d99ab82d3dc309cb
Ecequiel Chavez <ecequiel.chavez@dbtlabs.com>
2026-02-12 22:29:19 +0000

    staging models

--- models/modularity_practice/staging/stg_orders.sql
+++ models/modularity_practice/staging/stg_orders.sql
@@ -1,2 +1,17 @@
+with raw_orders as (
+    select * from {{ source("jaffle_shop", "ORDERS") }}
+    ),
+
+transformed as (
+    select
+                    row_number() over (
+                        partition by user_id order by order_date, id) as user_order_seq,
+                    id as order_id,
+                    user_id as customer_id,
+                    order_date,
+                    status as order_status
+                from raw_orders
+)
+
 select *
-from {{ source('jaffle_shop', 'ORDERS') }}
+from transformed
--- models/modularity_practice/staging/stg_payments.sql
+++ models/modularity_practice/staging/stg_payments.sql
@@ -1,2 +1,18 @@
+with raw_payments as (
+    select * from {{ source("stripe", "PAYMENT") }}
+    ),
+
+transformed as (
+    select 
+            ID as payment_id,
+            ORDERID as order_id,
+            PAYMENTMETHOD as payment_method,
+            STATUS as payment_status,
+            round(AMOUNT/100.0,2) as payment_amount,
+            CREATED as payment_created_at
+
+    from raw_payments
+)
+
 select *
-from {{ source('stripe', 'PAYMENT') }}
+from transformed
