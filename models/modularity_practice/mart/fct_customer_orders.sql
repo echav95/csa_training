7402cadcfc907020f595d5cf00b1421382bd4bc5
Ecequiel Chavez <ecequiel.chavez@dbtlabs.com>
2026-02-12 21:43:52 +0000

    k

--- models/modularity_practice/mart/fct_customer_orders.sql
+++ models/modularity_practice/mart/fct_customer_orders.sql
@@ -26,9 +26,11 @@ customers as (
 orders as (
         select
                     row_number() over (
-                        partition by user_id order by order_date, id
-                    ) as user_order_seq,
-                    *
+                        partition by user_id order by order_date, id) as user_order_seq,
+                    id as order_id,
+                    user_id as customer_id,
+                    order_date,
+                    status as order_status
                 from raw_orders
             ),
 
@@ -43,47 +45,47 @@ customer_order_history as (
             min(order_date) as first_order_date,
             min(
                 case
-                    when orders.status not in ('returned', 'return_pending') then order_date
+                    when orders.order_status not in ('returned', 'return_pending') then order_date
                 end
             ) as first_non_returned_order_date,
             max(
                 case
-                    when orders.status not in ('returned', 'return_pending') then order_date
+                    when orders.order_status not in ('returned', 'return_pending') then order_date
                 end
             ) as most_recent_non_returned_order_date,
             coalesce(max(user_order_seq), 0) as order_count,
             coalesce(
-                count(case when orders.status != 'returned' then 1 end), 0
+                count(case when orders.order_status != 'returned' then 1 end), 0
             ) as non_returned_order_count,
             sum(
                 case
-                    when orders.status not in ('returned', 'return_pending')
+                    when orders.order_status not in ('returned', 'return_pending')
                     then round(c.amount / 100.0, 2)
                     else 0
                 end
             ) as total_lifetime_value,
             sum(
                 case
-                    when orders.status not in ('returned', 'return_pending')
+                    when orders.order_status not in ('returned', 'return_pending')
                     then round(c.amount / 100.0, 2)
                     else 0
                 end
             ) / nullif(
                 count(
-                    case when orders.status not in ('returned', 'return_pending') then 1 end
+                    case when orders.order_status not in ('returned', 'return_pending') then 1 end
                 ),
                 0
             ) as avg_non_returned_order_value,
-            array_agg(distinct orders.id) as order_ids
+            array_agg(distinct orders.order_id) as order_ids
 
         from orders
 
         join customers
-            on orders.user_id = customers.customer_id
+            on orders.customer_id = customers.customer_id
 
-        left outer join raw_payments c on orders.id = c.orderid
+        left outer join raw_payments c on orders.order_id = c.orderid
 
-        where orders.status not in ('pending') and c.status != 'fail'
+        where orders.order_status not in ('pending') and c.status != 'fail'
 
         group by customers.customer_id, customers.full_name, customers.surname, customers.givenname
 
@@ -92,25 +94,25 @@ customer_order_history as (
 -- Final CTE
 
 final as (select
-    orders.id as order_id,
-    orders.user_id as customer_id,
+    orders.order_id as order_id,
+    orders.customer_id as customer_id,
     customers.surname,
     customers.givenname,
     first_order_date,
     order_count,
     total_lifetime_value,
     round(amount / 100.0, 2) as order_value_dollars,
-    orders.status as order_status,
+    orders.order_status as order_status,
     payments.status as payment_status
 from raw_orders as orders
 join customers
-    on orders.user_id = customers.customer_id
+    on orders.customer_id = customers.customer_id
 
 join customer_order_history
-    on orders.user_id = customer_order_history.customer_id
+    on orders.customer_id = customer_order_history.customer_id
 
 left outer join
-    raw_payments payments on orders.id = payments.orderid
+    raw_payments payments on orders.order_id = payments.orderid
 
 where payments.status != 'fail')
 
